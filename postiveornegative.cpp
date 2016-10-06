6623e1562e9f248d88a08c3639ba9280091dd12f
thayumanavan11 <thayumanavan11@gmail.com>
2016-10-06 15:19:25 +0530

    Update postiveornegative.cpp

--- postiveornegative.cpp
+++ postiveornegative.cpp
@@ -6,9 +6,9 @@ int main()
     int num;
     cin>>num;
     if(num>0)
-        cout<<"Positive";
+        cout<<"number is postive";
     else if(num<0)
-        cout<<"Negative";
+        cout<<"number is Negative";
     else 
         cout<<"Zero";
     return 0;
